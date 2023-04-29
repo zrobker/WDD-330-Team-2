fab84be7a41d56f3d26e8d0d8c578394344d3a59
emlemmon <lem19012@byui.edu>
2023-04-28 20:55:24 -0600

    debugged addProductToCart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,13 @@
-import { setLocalStorage } from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cart = getLocalStorage("so-cart");
+  if (!cart) {
+    cart = [];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -12,5 +17,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
