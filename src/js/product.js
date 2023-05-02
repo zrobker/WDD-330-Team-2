96f60a4b8e1b99e9c32378693253dc642e92de0a
emlemmon <lem19012@byui.edu>
2023-05-02 11:20:13 -0600

    fix productDetails.mjs and finished team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,6 @@
 import { getParam } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-import { addProductToCart, productDetails } from "./productDetails.mjs";
+import productDetails from "./productDetails.mjs";
 
-
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-const productId = getParam('product');
+const productId = getParam("product");
 
 productDetails(productId);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -3,18 +3,17 @@ import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
 
-
-export async function productDetails(productId) {
+export default async function productDetails(productId) {
     console.log("in productDetails");
     // get the details for the current product. findProductById will return a promise! use await or .then() to process it
     product = await findProductById(productId);
     // once we have the product details we can render out the HTML
     renderProductDetails();
     // once the HTML is rendered we can add a listener to Add to Cart button
-    document.getElementById("addToCart").addEventListener("click", addToCart);
+    document.getElementById("addToCart").addEventListener("click", addProductToCart);
 }
 
-export function addProductToCart(product) {
+export function addProductToCart() {
     console.log("in addProductToCart");
     let cart = getLocalStorage("so-cart");
     if (!cart) {
