490098faf0d4174ce3e99875ce89449ad9d606d8
emlemmon <lem19012@byui.edu>
2023-05-06 22:37:03 -0600

    fixed cart.html error when empty

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import { getParam } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
+import { getParam } from './utils.mjs';
+import productDetails from './productDetails.mjs';
 
-const productId = getParam("product");
+const productId = getParam('product');
 
 productDetails(productId);
