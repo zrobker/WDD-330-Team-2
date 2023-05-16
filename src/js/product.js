66d736470eff802a2084211b4edd995df19bfa80
DMergy <mer21040@byui.edu>
2023-05-15 20:04:57 -0700

    dynamic header and footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,9 @@
 import { getParam } from './utils.mjs';
 import productDetails from './productDetails.mjs';
 
+import { loadHeaderFooter } from "./utils.mjs";
+loadHeaderFooter();
+
 console.log("in product.js");
 const productId = getParam('product');
 
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,3 +1,5 @@
+
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
@@ -40,4 +42,45 @@ export function renderListWithTemplate(
   }
   const htmlString = list.map(templateFn);
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
+}
+
+
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = true
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+  if (callback) {
+    callback(data);
+  }
+}
+
+function loadTemplate(path) {
+  return async function () {
+        const res = await fetch(path);
+        if (res.ok) {
+        const html = await res.text();
+        return html;
+        }
+    };
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplateFn = loadTemplate("/partials/header.html");
+  const footerTemplateFn = loadTemplate("/partials/footer.html");
+
+  const headerEl = document.querySelector("#main-header");
+  const footerEl = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplateFn, headerEl);
+  renderWithTemplate(footerTemplateFn, footerEl);
+
 }
