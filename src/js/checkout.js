42eaf726e7bd23bc2b45734d0652f1cb996e1560
margoaston <margo.aston@yahoo.com>
2023-06-02 20:40:57 -0700

    error catching

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -13,8 +13,12 @@ document
   );
 
 // this is how it would look if we listen for the submit on the form
-document.forms['checkout'].addEventListener('submit', (e) => {
-  e.preventDefault();
-  // e.target would contain our form in this case
-  checkoutProcess.checkout(e.target);
-});
+document.querySelector('#checkoutSubmit')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    var myForm = document.querySelector("#order-form");
+    var chk_status = myForm.checkValidity();
+    myForm.reportValidity();
+    if(chk_status) 
+      checkoutProcess.checkout(myForm);
+  });
